d787548dcf4ed0ab5915de7e778d4136a1533560
Lars Yencken <lars@yencken.org>
2013-05-01 11:16:30 +1000

    Simplify detection of Python modules.

--- python.pl
+++ python.pl
@@ -3,27 +3,32 @@
 %  marelle-deps
 %
 
+%  All python packages are packages.
+pkg(P) :- python_pkg(P).
+
+%  If it's a Python package, it's met if you can import it.
+met(P, _) :-
+    python_pkg(P), !,
+    python_import(P).
+
+% python_import(+Pkg) is semidet.
+%   Try to import the module in Python, failing if the import fails.
 python_import(Pkg) :-
     join(['python -c \'import ', Pkg, '\' 2>/dev/null'], Cmd),
     shell(Cmd, 0).
 
-pkg(pandas).
-met(pandas, _) :- python_import(pandas).
-
-pkg(numpy).
-met(numpy, _) :- python_import(numpy).
-
-pkg(scipy).
-met(scipy, _) :- python_import(scipy).
-
-pkg('SimpleCV').
-met('SimpleCV', _) :- python_import('SimpleCV').
-
-pkg(virtualenv).
-met(virtualenv, _) :- python_import(virtualenv).
-
-pkg(networkx).
-met(networkx, _) :- python_import(networkx).
+python_pkg(pandas).
+python_pkg(numpy).
+python_pkg(scipy).
+python_pkg('SimpleCV').
+python_pkg(virtualenv).
+python_pkg(networkx).
+python_pkg(clint).
+python_pkg(flask).
+python_pkg(matplotlib).
 
 pkg(anytop).
 met(anytop, _) :- which(anytop).
+
+pkg(ipython).
+met(ipython, _) :- which(ipython).
