517b645a0b4d7457822a168e4874e8333b875ae2
Lars Yencken <lars@yencken.org>
2013-05-01 17:04:09 +1000

    Add haskell interpreters and compilers.

---
+++ haskell.pl
@@ -0,0 +1,12 @@
+%
+%  haskell.pl
+%  marelle-deps
+%
+
+command_pkg(hugs).
+installs_with_brew(hugs).
+installs_with_apt(hugs).
+
+command_pkg(ghc).
+installs_with_brew(ghc).
+installs_with_apt(ghc).
