223b6e62bd2e9a8d947f276d6b41555834a99e29
Lars Yencken <lars@yencken.org>
2013-05-02 10:06:28 +1000

    Make met check for pip packages using pip.

---
+++ 01-python-helpers.pl
@@ -0,0 +1,67 @@
+%
+%  01-python-helpers.pl
+%  marelle-deps
+%
+
+% python_pkg(-Pkg) is nondet.
+%   Pkg is a python module.
+:- multifile python_pkg/1.
+
+% pip_pkg(-Pkg) is nondet.
+%   Pkg is a python module installable with pip.
+:- multifile pip_pkg/1.
+
+% pip_pkg(-Pkg, -Type) is nondet.
+%   Pkg is a python module installable with pip. Type is either command
+%   or module.
+:- multifile pip_pkg/2.
+
+%  All python packages are packages.
+pkg(P) :- python_pkg(P).
+
+%  If it's a Python package, it's met if you can import it.
+met(P, _) :-
+    python_pkg(P), !,
+    python_import(P).
+
+% python_import(+Pkg) is semidet.
+%   Try to import the module in Python, failing if the import fails.
+python_import(Pkg) :-
+    join(['python -c \'import ', Pkg, '\' 2>/dev/null'], Cmd),
+    shell(Cmd, 0).
+
+%  All python packages depend on Python.
+depends(P, _, [python]) :-
+    python_pkg(P).
+
+%  All pip packages are also packages.
+pkg(P) :- pip_pkg(P).
+
+%  all pip packages depend on pip
+depends(P, _, [pip]) :- pip_pkg(P).
+
+met(P, _) :-
+    pip_pkg(P), !,
+    join(['pip show ', P], Cmd),
+    shellc(Cmd, Out),
+    atom_length(Out, L),
+    L > 0.
+
+%  meet pip packages on any platform by installing them with pip
+meet(P, _) :-
+    pip_pkg(P), !,
+    install_pip(P).
+
+% install_pip(+Pkg) is semidet.
+%   Try to install the pacakge with pip, maybe using sudo.
+install_pip(Pkg) :-
+    which(pip, Pip),
+    ( access_file(Pip, write) ->
+        Sudo = ''
+    ;
+        Sudo = 'sudo '
+    ),
+    join(['Installing ', Pkg, ' with pip'], Msg),
+    writeln(Msg),
+    join([Sudo, 'pip install ', Pkg], Cmd),
+    shell(Cmd, 0).
--- python.pl
+++ python.pl
@@ -3,11 +3,11 @@
 %  marelle-deps
 %
 
-%
-%  PACKAGES
-%
-
 command_pkg(python).
+
+command_pkg(pip).
+installs_with_apt(pip, python-pip).
+depends(pip, _, [python]).
 
 python_pkg(numpy).
 python_pkg(scipy).
@@ -19,56 +19,10 @@ pip_pkg(pandas).
 pip_pkg(virtualenv).
 pip_pkg(clint).
 pip_pkg(flask).
+pip_pkg(distribute).
+pip_pkg(anytop).
 
 command_pkg(ipython).
 installs_with_apt(ipython).
-installs_with_brew(ipython).
-
-command_pkg(anytop).
-meet(anytop, _) :- install_pip(anytop).
-
-
-%
-%  HELPERS
-%
-
-%  All python packages are packages.
-pkg(P) :- python_pkg(P).
-
-%  If it's a Python package, it's met if you can import it.
-met(P, _) :-
-    python_pkg(P), !,
-    python_import(P).
-
-%  All python packages depend on Python.
-depends(P, _, [python]) :-
-    python_pkg(P).
-
-% python_import(+Pkg) is semidet.
-%   Try to import the module in Python, failing if the import fails.
-python_import(Pkg) :-
-    join(['python -c \'import ', Pkg, '\' 2>/dev/null'], Cmd),
-    shell(Cmd, 0).
-
-%  all pip packages are also Python packages
-python_pkg(P) :- pip_pkg(P).
-
-%  all pip packages depend on pip
-depends(P, _, [pip]) :- pip_pkg(P).
-
-%  meet pip packages on any platform by installing them with pip
-meet(P, _) :- pip_pkg(P), !, install_pip(P).
-
-% install_pip(+Pkg) is semidet.
-%   Try to install the pacakge with pip, maybe using sudo.
-install_pip(Pkg) :-
-    which(pip, Pip),
-    ( access_file(Pip, write) ->
-        Sudo = ''
-    ;
-        Sudo = 'sudo '
-    ),
-    join(['Installing ', Pkg, ' with pip'], Msg),
-    writeln(Msg),
-    join([Sudo, 'pip install ', Pkg], Cmd),
-    shell(Cmd, 0).
+meet(ipython, osx) :- install_pip(ipython).
+depends(ipython, osx, [pip]).
