13ba05d566674f0149946a8e80db95570b09db25
Lars Yencken <lars@yencken.org>
2013-05-02 16:17:27 +1000

    Work around racket issue on OS X.

--- scheme.pl
+++ scheme.pl
@@ -8,5 +8,7 @@ installs_with_brew(chicken).
 installs_with_apt(chicken, 'chicken-bin').
 
 command_pkg(racket).
-installs_with_brew(racket, 'plt-racket').
+% Racket is broken on Homebrew currently: https://github.com/mxcl/homebrew/pull/17901
+% installs_with_brew(racket, 'plt-racket').
+installs_with_brew(racket, 'https://raw.github.com/kavu/homebrew/d23512a8fe2e7e77f0eef2e667d3846630112ab3/Library/Formula/plt-racket.rb').
 installs_with_apt(racket).
