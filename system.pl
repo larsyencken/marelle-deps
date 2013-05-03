c4d5d4d3c8870052e2b9a4e51483f4224e02d0f7
Lars Yencken <lars@yencken.org>
2013-05-03 17:28:42 +1000

    Add dotvim dep.

--- system.pl
+++ system.pl
@@ -5,10 +5,6 @@
 %  Basic system utility packages.
 %
 
-command_pkg(vim).
-installs_with_brew(vim).
-installs_with_apt(vim, 'vim-nox').
-
 command_pkg(htop).
 installs_with_brew(htop).
 installs_with_apt(htop).
---
+++ vim.pl
@@ -0,0 +1,25 @@
+%
+%  vim.pl
+%  marelle-deps
+%
+
+command_pkg(vim).
+installs_with_apt(vim, 'vim-nox').
+installs_with_brew(vim).
+
+meta_pkg('dotvim', [
+    '__dotvim checked out',
+    '__dotvim symlinked'
+]).
+
+git_step(
+    '__dotvim checked out',
+    'https://bitbucket.org/larsyencken/dotvim',
+    '~/.vim'
+).
+
+symlink_step(
+    '__dotvim symlinked',
+    '~/.vim/vimrc',
+    '~/.vimrc'
+).
