7e2f1b9699795743504df25f747c97913e5c4468
Lars Yencken <lars@yencken.org>
2013-05-05 16:41:21 +1000

    Deps for matplotlib and networkx.

--- python.pl
+++ python.pl
@@ -28,8 +28,6 @@ meta_pkg('python-recommended', [
 python_pkg(numpy).
 python_pkg(scipy).
 python_pkg('SimpleCV').
-python_pkg(matplotlib).
-python_pkg(networkx).
 
 pip_pkg(pandas).
 pip_pkg(virtualenv).
@@ -37,6 +35,7 @@ pip_pkg(clint).
 pip_pkg(flask).
 pip_pkg(distribute).
 pip_pkg(anytop).
+pip_pkg(networkx).
 
 command_pkg(ipython).
 installs_with_apt(ipython).
@@ -47,3 +46,6 @@ depends(ipython, osx, [pip]).
 %  https://github.com/wrobstory/vincent/
 python_pkg(vincent).
 meet(vincent, _) :- install_pip('https://github.com/wrobstory/vincent/archive/master.zip').
+
+python_pkg(matplotlib).
+installs_with_apt(matplotlib, 'python-matplotlib').
