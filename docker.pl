64a882b3eddaadd832f1d48df2cc652416196921
Lars Yencken <lars@yencken.org>
2013-05-09 14:31:30 +1000

    First cut at docker deps for linux.

--- docker.pl
+++ docker.pl
@@ -5,7 +5,18 @@
 %  LXC virtualisation.
 %
 
-git_step(docker,
+command_pkg(docker).
+installs_with_apt(docker, 'lxc-docker').
+depends(docker, linux(_), '__docker ppa').
+
+git_step('docker-source',
     'https://github.com/dotcloud/docker',
     '~/.local/docker'
 ).
+
+pkg('__docker ppa').
+met('__docker ppa', linux(_)) :-
+    isfile('/etc/apt/sources.list.d/docker.list').
+meet('__docker ppa', linux(CodeName)) :-
+    bash(['sudo sh -c "echo \'deb http://ppa.launchpad.net/dotcloud/lxc-docker/ubuntu ', CodeName, ' main\' >> /etc/apt/sources.list.d/docker.list"']),
+    bash('sudo apt-get update').
