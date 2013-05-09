f2c26da83322ed3f1e540c9f60df5f6007c948a3
Lars Yencken <lars@yencken.org>
2013-05-09 04:40:45 +0000

    Add ppa key and flavour check for docker.

--- docker.pl
+++ docker.pl
@@ -18,5 +18,7 @@ pkg('__docker ppa').
 met('__docker ppa', linux(_)) :-
     isfile('/etc/apt/sources.list.d/docker.list').
 meet('__docker ppa', linux(CodeName)) :-
+    member(CodeName, [precise, quantal]),
     bash(['sudo sh -c "echo \'deb http://ppa.launchpad.net/dotcloud/lxc-docker/ubuntu ', CodeName, ' main\' >> /etc/apt/sources.list.d/docker.list"']),
+    bash('sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E61D797F63561DC6'),
     bash('sudo apt-get update').
