b63433e9704961d0ef2dad6fff90c4dfb66e248d
Lars Yencken <lars@yencken.org>
2013-05-12 21:45:12 +1000

    Add x11 check for osx.

---
+++ x11.pl
@@ -0,0 +1,12 @@
+%
+%  x11.pl
+%  marelle-deps
+%
+
+pkg(x11).
+met(x11, osx) :- isdir('/Applications/XQuartz.app').
+meet(x11, osx) :-
+    writeln('Please download and install XQuartz manually'),
+    writeln('http://xquartz.macosforge.org/'),
+    !,
+    fail.
