fb0b28e3acc427ebc46579cba7845a0f55aa6da0
Lars Yencken <lars@yencken.org>
2013-11-12 14:11:05 +1100

    Add supervisor deps.

--- system.pl
+++ system.pl
@@ -12,3 +12,9 @@ managed_pkg(screen).
 command_pkg(pstree).
 installs_with_brew(pstree).
 installs_with_apt(pstree, psmisc).
+
+pkg(supervisor).
+installs_with_apt(supervisor).
+met(supervisor, osx) :- which(supervisorctl).
+meet(supervisor, osx) :- bash('pip install supervisor').
+depends(supervisor, osx, [pip]).
