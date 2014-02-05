df5192eaa0ca4a70945c24299f0dc1e9afaa7ba8
Lars Yencken <lars@yencken.org>
2014-02-06 10:35:52 +1100

    Add docker on osx.

--- docker.pl
+++ docker.pl
@@ -8,6 +8,11 @@
 command_pkg(docker).
 installs_with_apt(docker, 'lxc-docker').
 depends(docker, linux(_), '__docker ppa').
+depends(docker, osx, [boot2docker]).
+
+meet(docker, osx) :-
+    curl('http://get.docker.io/builds/Darwin/x86_64/docker-latest', '~/.local/bin/docker'),
+    bash('chmod a+x ~/.local/bin/docker').
 
 git_step('docker-source',
     'https://github.com/dotcloud/docker',
@@ -25,3 +30,9 @@ meet('__docker ppa', linux(CodeName)) :-
     writeln(AptLine),
     told,
     bash('sudo apt-get update').
+
+
+command_pkg(boot2docker).
+meet(boot2docker, osx) :-
+    curl('https://raw.github.com/steeve/boot2docker/master/boot2docker', '~/.local/bin/boot2docker'),
+    bash('chmod a+x ~/.local/bin/boot2docker').
