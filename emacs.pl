69f7ecad3bebae6bd39042c302f9b87269b3e798
Sébastien Le Callonnec <sebastien@weblogism.com>
2014-02-23 15:16:02 +0000

    Amend as per @larsyencken’s feedback.

--- emacs.pl
+++ emacs.pl
@@ -4,13 +4,10 @@
 %
 
 pkg(emacs).
-met(emacs, linux(_)) :-
-    isfile('/usr/local/bin/emacs').
+installs_with_apt('emacs').
+
 met(emacs, osx) :-
     isdir('/Applications/Emacs.app').
-
-meet(emacs, linux(_)) :-
-    installs_with_apt('emacs').
 meet(emacs, osx) :-
     bash('brew install --cocoa --srgb emacs'),
     bash('brew linkapps').
